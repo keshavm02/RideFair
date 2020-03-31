fd660fbb0648367341395923a1ea80ba9783bdb5
Keshav <km02@bu.edu>
2020-03-30 22:07:35 -0400

    added func for info search bar

---
+++ RideFair/InfoTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InfoTableViewController.swift
+//  RideFair
+//
+//  Created by Keshav Maheshwari on 3/30/20.
+//  Copyright © 2020 Keshav Maheshwari. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InfoTableViewController: UITableViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var infoSearchBar: UISearchBar!
+    @IBOutlet var tableViewOutlet: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        infoSearchBar.delegate = self
+        
+        tableViewOutlet.keyboardDismissMode = .interactive
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        infoSearchBar.setShowsCancelButton(true, animated: true)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        infoSearchBar.text = nil
+        infoSearchBar.setShowsCancelButton(false, animated: true)
+        infoSearchBar.endEditing(true)
+    }
+    
+}
