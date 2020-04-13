09e4726adad7ba9af1fae9c5ae2bff043bca265b
Keshav <km02@bu.edu>
2020-04-13 00:30:49 -0400

    set up dict for stopID

--- RideFair/InfoPageController.swift
+++ RideFair/InfoPageController.swift
@@ -13,6 +13,36 @@ class InfoPageController: UIViewController {
     
     let infoTableVC = InfoTableViewController()
     
+    let stopIDs: [String: String] = ["Lechmere": "place-lech",
+                                     "Science Park/West End": "place-spmnl",
+                                     "North Station": "place-north",
+                                     "Haymarket": "place-haecl",
+                                     "Government Center": "place-gover",
+                                     "Park Street": "place-pktrm",
+                                     "Boylston": "place-boyls",
+                                     "Arlington": "place-armnl",
+                                     "Copley": "place-coecl",
+                                     "Hynes Convention Center": "place-hymnl",
+                                     "Kenmore": "place-kencl",
+                                     "Blandford Street": "place-bland",
+                                     "BU East": "place-buest",
+                                     "BU Central": "place-bucen",
+                                     "BU West": "place-buwst",
+                                     "St Paul Street": "place-stplb",
+                                     "Pleasant Street": "place-plsgr",
+                                     "Babcock Street": "place-babck",
+                                     "Packard's Corner": "place-brico",
+                                     "Harvard Ave": "place-harvd",
+                                     "Griggs Street": "place-grigg",
+                                     "Allston Street": "place-alsgr",
+                                     "Warren Street": "place-wrnst",
+                                     "Washington Street": "place-wascm",
+                                     "Sutherland Road": "place-sthld",
+                                     "Chiswick Road": "place-chswk",
+                                     "Chestnut Hill Ave": "place-chill",
+                                     "South Street": "place-sougr",
+                                     "Boston College": "place-lake"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
