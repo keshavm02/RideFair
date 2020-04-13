807c5f79446da627e5e5d35aa3def9c6ce911b4a
Keshav <km02@bu.edu>
2020-04-13 00:16:17 -0400

    added new file infopagecontroller

---
+++ RideFair/InfoPageController.swift
@@ -0,0 +1,16 @@
+//
+//  InfoPageController.swift
+//  RideFair
+//
+//  Created by Keshav Maheshwari on 4/12/20.
+//  Copyright © 2020 Keshav Maheshwari. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InfoPageController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
